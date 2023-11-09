31060f8a5dcd06b9bba51a54d0315f82178bf92e
hahahahahahahahahahahahhaha1999 <148528205+hahahahahahahahahahahahhaha1999@users.noreply.github.com>
2023-11-09 23:24:44 +0900

    ユーザー管理機能修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     nickname { 'suzuki' }
-    email { 'test@example' }
-    password { '000000' }
-    password_confirmation { '000000' }
+    email {Faker::Internet.email}
+    password { '000aaa' }
+    password_confirmation { '000aaa' }
     last_name { '鈴木' }
     first_name { '太郎' }
     kana_last { 'スズキ' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,6 +58,27 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
+      it '英字のみのパスワードでは登録できない' do
+        @user.password ='aaaaaaaa'
+        @user.password_confirmation = 'aaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ああああああ'
+        @user.password_confirmation = 'ああああああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
       it 'お名前(全角)は、名字が空では登録できない' do
         @user.last_name = ''
         @user.valid?
