2051d5019de6b4ce50a0a218d8eb7a0ca11d60b2
hahahahahahahahahahahahhaha1999 <148528205+hahahahahahahahahahahahhaha1999@users.noreply.github.com>
2023-11-10 14:12:03 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
     validates :kana_last, :kana_first, format: { with: /\A[ァ-ヶー]+\z/, allow_blank: true }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, allow_blank: true }
   end
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, allow_blank: true }
 end
