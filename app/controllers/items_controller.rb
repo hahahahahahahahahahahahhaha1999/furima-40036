8f31ef16879a302885d937fc68e766caea23458c
hahahahahahahahahahahahhaha1999 <148528205+hahahahahahahahahahahahhaha1999@users.noreply.github.com>
2023-11-15 15:41:43 +0900

    商品購入機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id && @item.order.present?
+    if @item.user_id == current_user.id && @item.order.present? || current_user.id != @item.user_id
       redirect_to root_path
     end
   end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,17 +1,16 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :street_address, :building_name, :phone_number, :order, :token
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :street_address, :building_name, :phone_number, :token
   
 
   with_options presence: true do
-    validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :street_address, :phone_number
+    validates :user_id, :item_id, :post_code, :prefecture_id, :municipality, :street_address, :phone_number, :token
   end
 
   with_options numericality: { other_than: 0 } do
     validates :prefecture_id
   end
 
-  validates :token, presence: true
   validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
   validates :phone_number, format: {with: /\A\d{10,11}\z/, allow_blank: true}
 
